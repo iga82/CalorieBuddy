821973a0ab35593812d51f2ad840977aec7c7620
Irving Arredondo <irva91@gmail.com>
2015-01-20 23:02:29 -0800

    included jquery in index html

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 var calorie_dictionary = {
-  turkeyGroundBeef : 1.52
+  turkeyGroundBeef : 1.52,
+  ketchup : 20
 };
+
+// var myDiv = document.getElementById("myDiv");
+// //Create array of options to be added
+// var array = ["Turkey Ground Beef","Ketchup"];
+// //Create and append select list
+// var selectList = document.createElement("select");
+// selectList.id = "mySelect";
+// myDiv.appendChild(selectList);
+// //Create and append the options
+// for (var i = 0; i < array.length; i++) {
+//     var option = document.createElement("option");
+//     option.value = array[i];
+//     option.text = array[i];
+//     selectList.appendChild(option);
+// }
 
 /* GET home page. */
 // router.get('/', function(req, res) {
@@ -15,7 +31,7 @@ router.get('/', function(req, res){
   var result = calorie_dictionary.turkeyGroundBeef;
   res.render('index',{
     title: 'Hello World!',
-    age: result
+    age: result + calorie_dictionary.ketchup
   });
 
 });
