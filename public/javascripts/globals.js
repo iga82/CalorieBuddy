ed5006d5e7960c333227c505d01ea267da3543bf
Irving Arredondo <irva91@gmail.com>
2015-02-23 19:06:33 -0800

    fixed button width on amount

--- public/javascripts/globals.js
+++ public/javascripts/globals.js
@@ -2,40 +2,6 @@
 var total_calorie_count = 1;
 var textIndex = 0;
 
-// var calorie_dictionary = {
-//   avocado : 1.7,
-//   bread : 60,
-//   cheese : 3.22,
-//   chicken : 1.15,
-//   cuties : 40,
-//   eggWhites : 133.34,
-//   greekYo : 0.84,
-//   hKiss : 21.12,
-//   ketchup : 20,
-//   lunchMeat : 1.23,
-//   mushrooms : 0.25,
-//   pBeans : 0.85,
-//   pasta : 1.67,
-//   pastaSauce : 100,
-//   proteinShake : 130,
-//   rice : 0.92,
-//   shrimp : 1.06,
-//   sourCream : 1.33,
-//   soyMilk : 100,
-//   tortillaF : 80,
-//   tortillaC : 40,
-//   turkeyB : 30,
-//   turkeyGB : 1.08,
-//   turkeyS : 1.97,
-//   vCrackers : 4.67,
-//   mms : 4.62,
-//   gum : 3.5,
-//   rotisserieChicken : 1.65,
-//   smiles : 80,
-//   vanillaWafers : 17.5,
-//   pockySticks : 12.5
-// };
-
 var keyword_dictionary = {
   "Avocado"            : {serving: "grams", calories: 1.7},
   "Wheat Bread"        : {serving: "slices", calories: 60},
