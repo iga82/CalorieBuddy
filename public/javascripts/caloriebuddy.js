0a82e1339fcb0ce5c7dc0c077e992458b64cdc27
Irving Arredondo <irva91@gmail.com>
2016-01-09 21:23:14 -0600

    added validation for only integer input

--- public/javascripts/caloriebuddy.js
+++ public/javascripts/caloriebuddy.js
@@ -5,17 +5,30 @@ $(function(){
         event.preventDefault();
 
         // json object housing the nutrition info
-        var nutritionInfo = {};
+        var nutritionInfo = null;
 
         // grab inputs
         var carbInput = $("#carb-input").val();
-        var proteinInput = $("#protein-input").val()
+        var proteinInput = $("#protein-input").val();
         var fatInput = $("#fat-input").val();
 
         if(!carbInput || !proteinInput || !fatInput){
+            $("#macro-info").hide();
+            $("#macro-input").addClass("has-error");
+        }
+        else if(!isInteger(carbInput) || !isInteger(proteinInput) || !isInteger(fatInput)){
+            $("#macro-info").hide();
             $("#macro-input").addClass("has-error");
         }
         else{
+            // fill in nutrition info
+            nutritionInfo = {};
+            nutritionInfo.carbCalories = parseInt(carbInput) * 4;
+            nutritionInfo.proteinCalories = parseInt(proteinInput) * 4;
+            nutritionInfo.fatCalories = parseInt(fatInput) * 4;
+        }
+
+        if(nutritionInfo){
             $("#macro-input").removeClass("has-error");
             var carbCalories = $("#carb-input").val() * 4;
             var proteinCalories = $("#protein-input").val() * 4;
@@ -28,5 +41,10 @@ $(function(){
             $("#macro-info").show();
         }
     });
+
+    function isInteger (numStr){
+        // test num string to see if integer
+        return /^([0-9]\d*)$/.test(numStr);
+    }
 
 });
