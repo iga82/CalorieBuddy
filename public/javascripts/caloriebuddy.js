100cb843d70b924aafbfb09835f6e7966a4e8267
Irving Arredondo <irva91@gmail.com>
2016-01-09 20:44:00 -0600

    added validation for missing input

--- public/javascripts/caloriebuddy.js
+++ public/javascripts/caloriebuddy.js
@@ -3,15 +3,30 @@ $(function(){
 
     $("#done").click(function(){
         event.preventDefault();
-        var carbCalories = $("#carb-input").val() * 4;
-        var proteinCalories = $("#protein-input").val() * 4;
-        var fatCalories = $("#fat-input").val() * 9;
-        var calorieTotal = carbCalories + proteinCalories + fatCalories;
 
-        $("#carb-percentage").text("Carbs: " + Math.round((carbCalories / calorieTotal) * 100) + "%");
-        $("#protein-percentage").text("Protein: " + Math.round((proteinCalories / calorieTotal) * 100) + "%");
-        $("#fat-percentage").text("Fat: " + Math.round((fatCalories / calorieTotal) * 100) + "%");
-        $("#macro-info").show();
+        // json object housing the nutrition info
+        var nutritionInfo = {};
+
+        // grab inputs
+        var carbInput = $("#carb-input").val();
+        var proteinInput = $("#protein-input").val()
+        var fatInput = $("#fat-input").val();
+
+        if(!carbInput || !proteinInput || !fatInput){
+            $("#macro-input").addClass("has-error");
+        }
+        else{
+            $("#macro-input").removeClass("has-error");
+            var carbCalories = $("#carb-input").val() * 4;
+            var proteinCalories = $("#protein-input").val() * 4;
+            var fatCalories = $("#fat-input").val() * 9;
+            var calorieTotal = carbCalories + proteinCalories + fatCalories;
+
+            $("#carb-percentage").text("Carbs: " + Math.round((carbCalories / calorieTotal) * 100) + "%");
+            $("#protein-percentage").text("Protein: " + Math.round((proteinCalories / calorieTotal) * 100) + "%");
+            $("#fat-percentage").text("Fat: " + Math.round((fatCalories / calorieTotal) * 100) + "%");
+            $("#macro-info").show();
+        }
     });
-    
+
 });
