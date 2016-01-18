ab6edd96877f00235937385eca5792321b18f62f
Irving Arredondo <irva91@gmail.com>
2016-01-17 23:11:43 -0600

    added new comment

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // how we tell our routes to get used
 app.use('/', routes);
-
+// new comment - irving
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
